a7e8bdde86862c2364350485e373772e596423ad
Nazmi Yazkan <nyazkan@gtu.edu.tr>
2019-07-13 21:04:05 +0300

    Updates

---
+++ precache-manifest.2eb90df370c6ca1945ea3d50a857bc39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "839d01f42b35695c1fb0117b4fb0dd1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c74f285d1bae8dc5562a",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3be81b5c0892e252c6f2",
+    "url": "/static/js/2.e84f2bdc.chunk.js"
+  },
+  {
+    "revision": "c74f285d1bae8dc5562a",
+    "url": "/static/js/main.329c4a2b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
